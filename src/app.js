a68c090a8844015979ff549d725462703427dec7
kimjaewoo1207 <a01024308988@gmail.com>
2026-09-16 11:18:36 +0900

    프론트엔드 프로그래밍 수업 환경 구성

---
+++ src/app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `hello, ${name}`;
+}
+const message = greet("TypeScript");
+console.log(message);
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+function greet(name: string): string {
+return `hello, ${name}`;
+}
+const message = greet("Typescript");
+console.log(message);
